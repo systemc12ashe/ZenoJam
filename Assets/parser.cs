258e9074769498782cff06d4a124871042502e12
Cassidy <62119269+systemc12ashe@users.noreply.github.com>
2022-05-29 14:46:55 -0400

    parser/conversation file updates

--- Assets/parser.cs
+++ Assets/parser.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
@@ -20,21 +21,34 @@ public class parser : MonoBehaviour
         
     }
 
-    public List<string> GetConversationText(int conversationNumber)
+    public string[] conversationText;
+    public List<string> introductions;
+    public List<string> beatrice;
+    public List<string> sally;
+    public List<string> brad;
+    public List<string> lynus;
+    [ContextMenu("Test Parser")]
+    public void GetConversationText()
     {
         string path = AssetDatabase.GetAssetPath(conversations);
-        string[] conversationText;
+        
         using (StreamReader sr = new StreamReader(path))
         {
             var output = sr.ReadToEnd();
-            conversationText = output.Split(new string[] {"***"}, System.StringSplitOptions.None);
-            string currentCharacter = conversationText[conversationNumber].Substring(0,1);
-            string currentConversationText = conversationText[conversationNumber].Substring(2);
-            Debug.Log(currentConversationText + currentCharacter);
-            List<string> results = new List<string>();
-            results.Add(currentCharacter);
-            results.Add(currentConversationText);
-            return (results);
+            conversationText = output.Split(new string[] { "***" }, System.StringSplitOptions.None);
+            AddToList(introductions, 0, 5);
+            AddToList(beatrice, 6, 19);
+            AddToList(lynus, 20, 33);
+            AddToList(brad, 34, 50);
+            AddToList(sally, 51, 63);
+        }
+    }
+
+    public void AddToList(List<string> currentList,int start, int end)
+    {
+        for (int i = start; i <= end; i++)
+        {
+            currentList.Add(conversationText[i]);
         }
     }
 }
