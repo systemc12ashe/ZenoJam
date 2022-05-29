fd12f780cf593e4186ab88ec1466377067fcd470
SalteWalte <61295061+WalterKeiler@users.noreply.github.com>
2022-05-29 14:58:30 -0400

    Update GameManager.cs, parser.cs, and packages-lock.json

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -27,8 +27,6 @@ public class Character
     public string name;
     public float attractiveness = 0;
     public int lineNum = 0;
-    public string[] interaction1;
-    public string[] interaction2;
-    public string[] interaction3;
+    public string[] lines;
     public Sprite background;
 }
